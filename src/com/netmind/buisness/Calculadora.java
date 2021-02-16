890c345ae05eaf372af8f5dd4caf429c99dfcc99
DESKTOP-QB2CK6H\Carmine <carmine-bnt@hotmail.com>
2021-02-16 21:06:24 +0100

    initial commit, with calculator structure.

---
+++ src/com/netmind/buisness/Calculadora.java
@@ -0,0 +1,22 @@
+package com.netmind.buisness;
+
+public class Calculadora {
+	
+	public int add (int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int add (int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public int substract(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public int divide (int num1, int num2) {
+		num1 / num2;
+	}
+	
+	
+}
---
+++ src/com/netmind/buisness/integration/test/CalculadoraIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.netmind.buisness.integration.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraIntegrationTest {
+
+	@Test
+	public void testAdd() {
+		Calculadora calculadora = new Calculadora()
+		assertEquals(calculadora.add(4, 2), 6))
+	}
+
+	@Test
+	public void testSubstract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+}
