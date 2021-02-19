9c635021d1d8ecf07f182e5eeaa7a6333d24a132
DESKTOP-QB2CK6H\Carmine <carmine-bnt@hotmail.com>
2021-02-19 20:47:47 +0100

    Studen model created with toString, Equals and Hashcode

--- src/com/netmind/buisness/Calculator.java
+++ src/com/netmind/buisness/Calculator.java
@@ -6,11 +6,11 @@ public class Calculator {
 		return num1 + num2;
 	}
 	
-	public int add (int num1, int num2) {
+	public int substract (int num1, int num2) {
 		return num1 - num2;
 	}
 	
-	public int substract(int num1, int num2) {
+	public int multiply (int num1, int num2) {
 		return num1 * num2;
 	}
 	
